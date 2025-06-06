dabffd0ec4e8571ff0ce57f792d5b668be4c8a95
cdkodi <85918578+cdkodi@users.noreply.github.com>
2025-06-06 10:04:19 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from './components/Hero'
+import FeaturedTemples from './components/FeaturedTemples'
+import TraditionCards from './components/TraditionCards'
+import RecentArticles from './components/RecentArticles'
+import CallToAction from './components/CallToAction'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturedTemples />
+      <TraditionCards />
+      <RecentArticles />
+      <CallToAction />
+    </>
+  )
+}
