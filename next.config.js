321b754c620f30fe6d256a24502cf7acdfbbcc59
cdkodi <85918578+cdkodi@users.noreply.github.com>
2025-06-06 10:23:20 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'lh3.googleusercontent.com'],
     formats: ['image/webp', 'image/avif'],
   },
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
